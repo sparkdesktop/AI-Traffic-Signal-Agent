ce7050b37f2f4395d3833bfa41ad54e3388dbc36
sparkdesktop <sparkdesktop@proton.me>
2023-07-21 16:54:17 +0000

    AAAAAAAAAAARGH!

--- Deep_Learning-based Agent/src/agent/agent.java
+++ Deep_Learning-based Agent/src/agent/agent.java
@@ -23,4 +23,5 @@ public class agent  extends Agent {
      
     mainn m=new mainn(4,5,6,8);
     public void setup(){
-        System.out.println("Hola 
+        System.out.println("Hola Mundo");
+        gui gui = new gui(th
