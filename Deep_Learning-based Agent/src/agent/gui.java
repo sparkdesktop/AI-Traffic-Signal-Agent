93b82866dff91f75c9391d74ce2246aac82c12ab
sparkdesktop <sparkdesktop@proton.me>
2023-12-20 06:07:10 +0000

    initial project commit

--- Deep_Learning-based Agent/src/agent/gui.java
+++ Deep_Learning-based Agent/src/agent/gui.java
@@ -86,4 +86,4 @@ public class gui extends JFrame {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int centerX = (int)screenSize.getWidth() / 2;
 		int centerY = (int)screenSize.getHeight() / 2;
-		setLocation(centerX - getWidth() / 
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 
