02f394df076934a9fa727fbc3931b22598277597
sparkdesktop <sparkdesktop@proton.me>
2023-12-22 17:54:52 +0000

    13.0.0

--- Deep_Learning-based Agent/src/ann/activacion.java
+++ Deep_Learning-based Agent/src/ann/activacion.java
@@ -1,3 +1,4 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, ch
+ * To change this template file, choose Tools | Templates
+ * and open t
