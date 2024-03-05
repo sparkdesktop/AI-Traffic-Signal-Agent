546444cb6defce3ee5ff892c17908d6a4d02031c
sparkdesktop <sparkdesktop@proton.me>
2024-03-05 12:08:45 +0000

    Delete help.lua

--- Deep_Learning-based Agent/src/ann/calculos.java
+++ Deep_Learning-based Agent/src/ann/calculos.java
@@ -20,3 +20,31 @@ public class calculos {
     activacion [] neuronO, neuronH;
     static double balance=-1;
     
+    public calculos(int nodosCapaO, int nodosCapaH, int entCapaO, int entraCapaH ){
+        this.nodosCapaO= nodosCapaO;
+        this.nodosCapaH= nodosCapaH;
+        this.entCapaO=entCapaO;
+        this.entCapaH=entraCapaH;
+    }
+    
+    public void crearRed(){
+        
+        //capa O
+        neuronO =new activacion[nodosCapaO];
+        for(int o=0;o<nodosCapaO;o++){
+           neuronO[o]=new activacion(entCapaO);
+        }
+        //capa H
+        neuronH =new  activacion[nodosCapaH];
+        for(int h=0;h<nodosCapaH;h++){
+           neuronH[h]=new activacion(entCapaH);           
+        }
+    }
+    public void inicioPesos(){
+        //capa o
+        for(int o=0;o<nodosCapaO;o++){
+            for(int i=0;i<entCapaO ;i++){
+               neuronO[o].pesos[i]=Math.random();
+            }
+           // neuronO[o].verPesos();
+        }
