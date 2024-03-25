deeb747584fc16b2f2568b0f6317b8db026ef157
sparkdesktop <sparkdesktop@proton.me>
2024-03-25 10:47:35 +0000

    add class

--- Deep_Learning-based Agent/src/ann/sigmoide.java
+++ Deep_Learning-based Agent/src/ann/sigmoide.java
@@ -1,2 +1,3 @@
 /*
- * To change this license header, choose License Headers in Project Pro
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file,
