c93c3e205b77f7634091e206cc17345eccab28b0
sparkdesktop <sparkdesktop@proton.me>
2024-04-05 17:30:05 +0000

    add usage to readme

--- Deep_Learning-based Agent/src/digitos/Digit.java
+++ Deep_Learning-based Agent/src/digitos/Digit.java
@@ -12,3 +12,33 @@ import ann.calculos;
 /**
  *
  * @author celes
+ */
+public class Digit {
+    static matriz m=new matriz();
+    static int nodoso=4;
+    static int nodosh=5;
+    static int entCapaO =6, entraCapaH=8 ;
+    
+    static  calculos redNeuronal=new calculos(nodoso,nodosh,entCapaO,entraCapaH);
+    
+    public static void main(String[] args) {
+        redNeuronal.crearRed();
+        redNeuronal.inicioPesos();
+        
+        int numeroIter=70000; //n
+        int i=0;
+        int iteracion;//epoca
+        double error;
+        int numeroactualItera=0;//k
+        boolean noigual=false;
+        while(i<m.getCapaI().length && numeroactualItera < numeroIter ){
+             iteracion=i;
+            if(i==0){
+                 System.out.println("---------------------------------------------------Iteracion "+ numeroactualItera+" ------------------------");
+            }
+             
+            for (int j = 0; j < 7; j++) {
+                 System.out.print(""+Math.round(m.getCapaI()[iteracion][j])+" ");
+                
+            }
+            System.out.print(" Aprender ");
