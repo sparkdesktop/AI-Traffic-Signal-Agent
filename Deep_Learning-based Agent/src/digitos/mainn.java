bfc1f7cb11a25eb8b4543bb0dae4fc980451a89a
sparkdesktop <sparkdesktop@proton.me>
2024-04-17 22:41:46 +0000

    TODO-721: WIP

--- Deep_Learning-based Agent/src/digitos/mainn.java
+++ Deep_Learning-based Agent/src/digitos/mainn.java
@@ -3,3 +3,28 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ */
+package digitos;
+
+import ann.calculos;
+import digitos.matriz;
+
+/**
+ *
+ * @author celes
+ */
+public class mainn {
+     static matriz m=new matriz();
+    static int nodoso;
+    static int nodosh;
+    static int entCapaO , entraCapaH;
+    
+    static  calculos redNeuronal=new calculos(nodoso,nodosh,entCapaO,entraCapaH);
+    
+    public mainn (int nodoso, int nodosh, int entCapaO, int entraCapaH){
+    this.nodoso=nodoso;
+    this.nodosh=nodosh;
+    this.entCapaO =entCapaO;
+    this.entraCapaH=entraCapaH ;
+    }
+   
